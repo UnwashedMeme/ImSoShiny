c1c0f729b3c8a88c6ea5a9fbbbf4ac51ebd0598e
Nathan Bird <ecthellion@gmail.com>
2017-08-08 22:27:20 -0400

    Little bit closer to validation being sorted

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -32,7 +32,7 @@ let noble1 = {
 [<Tests>]
 let playerTests =
      testList "Player behavior" [
-        test "Victory Points-0" {
+        testProperty "Victory Points-0" {
             Expect.equal emptyPlayer.VictoryPoints 0 "An empty player has no victory Points";
         };
         test "Victory Points >0" {
@@ -71,59 +71,14 @@ let updateGameStateTests =
         tiershow = [];
         nobles = [];
     }
-    let action = Draw2OfSame (Coin.Gem Green)
+    let action = MainAction (Draw2OfSame (Coin.Gem Green))
     testList "PlayerAction Validators" [
         testList "validateCorrectPlayer" [
             test "Right player" {
-                Expect.equal (validateCorrectPlayer gameState (player1, action)) None "The player1 is up, player1 sent action"
+                Expect.equal (validateCorrectPlayer gameState player1 action) None "The player1 is up, player1 sent action"
             };
             test "Wrong player" {
-                match (validateCorrectPlayer gameState (player2, action)) with
-                    | None -> failwith "reported no errors for wrongplayer"
-                    | Some (ServerEvent.InvalidAction (returnPlayer, returnAction, reason)) -> 
-                        Expect.equal returnPlayer player2 "reported back to the wrong player"
-                        Expect.equal returnAction action "reported back the wrong action" 
-                    | Some (other) -> failwith (sprintf "Got %A" other)
-            };
-        ];
-        testList "validate Draw2OfSame" [
-            test "Not This Action" {
-                Expect.isNone (validateDraw2OfSame gameState (player1, Draw3Different (Coin.Gem White, Coin.Gem Blue, Coin.Gem Green))) "Shouldn't be commenting on this action type"
-            };
-            test "Bank has None" {
-                Expect.isSome (validateDraw2OfSame gameState (player1, Draw2OfSame (Coin.Gem White))) "There aren't two whites in bank"
-            };
-            test "Bank has one" {
-                let bank = { inventory = [Coin.Gem White]}
-                let gameState = { gameState with bank = bank }
-                Expect.isSome (validateDraw2OfSame gameState (player1, Draw2OfSame (Coin.Gem White))) "There aren't two whites in bank"
-            };
-            test "Bank has enough" {
-                let bank = { inventory = [Coin.Gem White; Coin.Gem White]}
-                let gameState = { gameState with bank = bank }
-                Expect.isNone (validateDraw2OfSame gameState (player1, Draw2OfSame (Coin.Gem White))) "There were enough coins in bank"
-            };
-        ];
-
-        testList "validate Draw3Different" [
-            test "Not This Action" {
-                Expect.isNone (validateDraw3Different gameState (player1, Draw2OfSame (Coin.Gem White))) "Shouldn't be commenting on this action type"
-            };
-            test "Bank has None" {
-                let draw3action = Draw3Different (Coin.Gem White, Coin.Gem Blue, Coin.Gem Green)
-                Expect.isSome (validateDraw3Different gameState (player1, draw3action)) "There aren't enough coins in bank"
-            };
-            test "Bank has one" {
-                let draw3action = Draw3Different (Coin.Gem White, Coin.Gem Blue, Coin.Gem Green)
-                let bank = { inventory = [Coin.Gem White]}
-                let gameState = { gameState with bank = bank }
-                Expect.isSome (validateDraw3Different gameState (player1, draw3action)) "There aren't two whites in bank"
-            };
-            test "Bank has enough" {
-                let draw3action = Draw3Different (Coin.Gem White, Coin.Gem Blue, Coin.Gem Green)
-                let bank = { inventory = [Coin.Gem White; Coin.Gem White; Coin.Gem Blue; Coin.Gem Green]}
-                let gameState = { gameState with bank = bank }
-                Expect.isNone (validateDraw3Different gameState (player1, draw3action)) "There were enough coins in bank"
+                Expect.isSome (validateCorrectPlayer gameState player2 action) "On the wrong player it should say so"
             };
         ];
         
--- src/Server.fs
+++ src/Server.fs
@@ -10,17 +10,21 @@ type ServerState =
     | FinishingTurn
     | EndGame
 
-let validateCorrectPlayer (gamestate:GameState) fromPlayer (action:Action) =
+let validateCorrectPlayer (gamestate:GameState) fromPlayer (action:Action) : string option  =
     let playerUp = List.head gamestate.players
     if playerUp <> fromPlayer then
-        Some (ServerEvent.InvalidAction(fromPlayer, action, "Not your turn"))
+        Some "Not your turn"
     else
         None
 
-let validatePhase gamestate player action : ServerEvent option = 
-    Some (ServerEvent.InvalidAction (player, action, "Wrong phase"))
+let validatePhase gamestate player action  = 
+    Some "Wrong phase"
 
-
+let bindValidator validator gamestate player action =
+    function 
+        | Some err ->  Some err
+        | None -> validator gamestate player action
+    
 let draw2OfSame (gamestate:GameState) (player:Player) (coin:Coin) =
     ServerEvent.GameStateUpdate gamestate
 
@@ -32,6 +36,13 @@ let buyCard gamestate player card =
 let reserveCard gamestate player card = 
     ServerEvent.GameStateUpdate gamestate
 
+let discardCoins gamestate player discardCoinsAction = 
+    ServerEvent.GameStateUpdate gamestate
+
+let buyNoble gamestate player buyNobleAction =
+    ServerEvent.GameStateUpdate gamestate
+    
+
 let mainAction gamestate player mainaction : ServerEvent = 
     match mainaction with
         | MainAction.Draw2OfSame coin -> draw2OfSame gamestate player coin
@@ -41,10 +52,12 @@ let mainAction gamestate player mainaction : ServerEvent =
 
 
 let receiveCommand (gamestate:GameState) player (action:Action): ServerEvent =
-    match (validateCorrectPlayer gamestate player action) with 
-        | Some se -> se
-        | None -> (match action with
-                    | Action.MainAction mainaction 
-                        -> validatePhase gamestate player action
-                           mainAction gamestate player mainaction 
-                )
+    let validators = [validateCorrectPlayer; validatePhase]
+    
+    match validators |> Seq.tryPick (fun v-> v gamestate player action) with 
+        | Some reason -> ServerEvent.InvalidAction (player,action,reason)
+        | None -> match action with
+                    | Action.MainAction ma -> mainAction gamestate player ma 
+                    | Action.DiscardCoinsAction dca-> discardCoins gamestate player dca
+                    | Action.BuyNobleAction bna -> buyNoble gamestate player bna
+                
